b36ad476d426ebd59e6ed505e1c9992869f1f85e
Abid Bakhtiyar <abidbakhtiyar@csu.fullerton.edu>
2020-04-29 20:12:02 -0700

    Implemented a put function to add files and added functions for getting file information

--- Client.py
+++ Client.py
@@ -1,58 +1,109 @@
 from socket import *
 from random import randint
 from random import seed
+
 import sys
-#user supplied values, command line arguments
+
+# user supplied values, command line arguments
 try:
     serverName = sys.argv[1]
     serverPort = sys.argv[2]
 
-#default values
+# default values
 except:
     serverName = "localhost"
     serverPort = 21
 
-controlSocket = socket(AF_INET,SOCK_STREAM)
+controlSocket = socket(AF_INET, SOCK_STREAM)
 
-#Connect to Server, assumed Server is listening
-controlSocket.connect((serverName,serverPort))
+# Connect to Server, assumed Server is listening
+controlSocket.connect((serverName, serverPort))
 
-#client sends a unique value to authenticate
-#this value is also a session id
-#convert random int to string
+# client sends a unique value to authenticate
+# this value is also a session id
+# convert random int to string
 seed(1)
-userToken = str(randint(1,1000))
+userToken = str(randint(1, 1000))
 
-dataPort = 0;
-def connect_to_server():
+dataPort = 0
+
+
+def testCase():
+    print("works")
+
+
+def establish_control_connection():
+    global dataPort
     bytesSent = 0
     while bytesSent != len(userToken):
-
         bytesSent += controlSocket.send(userToken.encode()[bytesSent:])
 
-        #recieve response from server
-        dataPort = controlSocket.recv(1024)
+        # recieve response from server
+        dataSize = bytes()
+        dataPort = controlSocket.recv(1024).decode()
 
-    print("Data port connection at: ", dataPort.decode())
+    print("Establishing Control Connection: ", dataPort)
 
-#-----main-------------------
 
-#establish connection
-connect_to_server();
+def getFileInfo(filename):
+    fileObj = open(filename, "r")
+    fileData = fileObj.read(65536)
+    filesize = 0
+    testCase()
+    if fileData:
+        filesize = str(len(fileData))
+        while len(filesize) < 10:
+            filesize = "0" + filesize
+            print(filesize + " the file size")
+            testCase()
+
+    fileData = filesize + fileData
+    testCase()
+    return fileData
+
+
+def list_files():
+    # send 'ls' to the server
+    controlSocket.send(userInput.encode())
+    # server responds with the new data port
+    dataPort = int(controlSocket.recv(1024).decode())
+    # Connect to Data Socket on server
+    dataSocket = socket(AF_INET, SOCK_STREAM)
+    dataSocket.connect((serverName, dataPort))
+    # wait for server to send requested list of files
+    msg = dataSocket.recv(1024).decode()
+    print(msg)
+
+    dataSocket.close()
+
+
+# -----main-------------------
+
+# establish connection
+establish_control_connection();
 
 keep_open = True
 while keep_open:
-    userInput = input("ftp>")
-    if userInput == 'quit':
+    userInput = input("ftp> ")
+    user = userInput.split(" ")
+    if user[0] == 'quit':
         keep_open = False
-        break;
-    if userInput== 'get':
-        break;
-        # dataSocket = socket(AF_INET, SOCK_STREAM)
-        # dataSocket.connect((serverName, dataPort))
-        # filename = 'myFile'
-        # dataSocket.send(filename.encode())
-        # statusCode = dataSocket.recv(1024);
-        # print(statusCode)
+    elif user[0] == 'get':
+        break
+    elif user[0] == 'put':
+        filename = user[1]
+        # print(filename)
+        fileinfo = getFileInfo(filename)
+        controlSocket.send(userInput.encode())
+        newPort = int(controlSocket.recv(1024).decode())
+        newSock = socket(AF_INET, SOCK_STREAM)
+        newSock.connect((serverName, newPort))
+        # print(fileinfo + "The file info")
+        numBytes = 0
+        while len(fileinfo) > numBytes:
+            numBytes += newSock.send(fileinfo[numBytes].encode())
+
+    elif user[0] == 'ls':
+        list_files()
 
 controlSocket.close()
--- Server.py
+++ Server.py
@@ -1,74 +1,125 @@
-#server code
+# server code
+
+
 from socket import *
 import sys
+from os import listdir, path
 
-#user supplied values, command line arguments
+# user supplied values, command line arguments
 try:
-    #the port on which to listen
-    serverPort=sys.argv[1]
+    # the port on which to listen
+    serverPort = sys.argv[1]
 
-#default port number
+# default port number
 except:
     serverPort = 21
 
-#create a TCP socket
-serverSocket = socket(AF_INET,SOCK_STREAM)
+# create a TCP socket
+serverSocket = socket(AF_INET, SOCK_STREAM)
 
-#bind the connection to the port
-serverSocket.bind(('',serverPort))
+# bind the connection to the port
+serverSocket.bind(('', serverPort))
 
-#start listening for incoming connections
+# start listening for incoming connections
 serverSocket.listen(1)
-print("The server is ready to recieve on port number: " + str(serverPort) )
+print("The server is ready to recieve on port number: " + str(serverPort))
 
-#the buffer to store the recieved data
+# the buffer to store the recieved data
 data = ""
 
-#store user access token and associated port number
-userPort= {}
+# store user access token and associated port number
+userPort = {}
 
-def generate_ephemeral_port():
-    dataPort = socket(AF_INET, SOCK_STREAM)
-    dataPort.bind(('',0))
-    return dataPort
 
-class Datagram:
-    def __init__(self,message):
-        self.message = message
+# Generate and open data connection
+def generate_ephemeral_port(connectionSocket):
+    dataSocket = socket(AF_INET, SOCK_STREAM)
+    dataSocket.bind(('', 0))
 
-#forever accept incoming connections
-#Establish Control Connection
-while 1:
-    connectionSocket, addr = serverSocket.accept()
+    # Send Data port number back to client
+    dataPort = str(dataSocket.getsockname()[1])
+    connectionSocket.send(dataPort.encode())
+    print("Server chose ephemeral port: ", dataPort)
 
-    #temporary buffer
-    tmpBuff=""
-    while len(data) != 1024:
-        #receive whatever the newly connected client has to send
-        tmpBuff = connectionSocket.recv(1024).decode()
-        # The other side unexpectedly closed it's socket
+    # store client token
+    userPort[data] = dataPort;
+
+    dataSocket.listen(1);
+    dataConnection, addr = dataSocket.accept()
+    return dataConnection
+
+
+def mkFile(filename, fileData):
+    newPath = "C:/Users/abidb/Documents/File-Transfer-Protocol/File-Transfer-Protocol/userFiles/" + cmd[1]
+    f = open(newPath, "w+")
+    f.write(fileData)
+    f.close()
+
+
+def recFile(sock, bytes):
+    recBuff = ""
+    tmpBuff = ""
+    # print(bytes)
+    while len(recBuff) < bytes:
+        tmpBuff = sock.recv(bytes)
+        # print(tmpBuff)
         if not tmpBuff:
             break
+        recBuff += tmpBuff.decode()
+        # print(recBuff)
 
-        #save cleint data, expecting a unique token
-        data += tmpBuff
-        #print out what the client sent
-        print('client token: ' + data)
+    return recBuff
 
-        #process message to send back to client
-        data = data.replace("client","server")
-        obj = Datagram(data.encode())
-        data = ""
 
-        #generate ephemeral port
-        dataPort = generate_ephemeral_port()
-        dataPort_str = str(dataPort.getsockname()[1])
+def list_files():
+    # connectionSocket, addr = dataPort.accept()
+    fileList = listdir('./userFiles')
+    files = " "
+    return files.join(fileList)
 
-        #store client token
-        userPort[data] = dataPort_str;
 
-        print("Server chose ephemeral port: ", dataPort_str)
-        connectionSocket.send(dataPort_str.encode())
+def send_data(socket, data):
+    socket.send(data.encode())
 
+
+def testcase():
+    print("works")
+
+
+# Establish Control Connection
+connectionSocket, addr = serverSocket.accept()
+data = connectionSocket.recv(1024).decode()
+print('client token: ' + data)
+status = '200OK'
+send_data(connectionSocket, status)
+
+# forever accept incoming connections
+while 1:
+    msg = connectionSocket.recv(1024).decode()
+    cmd = msg.split(" ")
+    if cmd[0] == 'ls':
+        print('Listing all files')
+        dataConnection = generate_ephemeral_port(connectionSocket)
+        files = list_files()
+        dataConnection.send(files.encode())
+        dataConnection.close()
+        testcase()
+    elif cmd[0] == 'put':
+        print('Receiving files')
+        dataConnection = generate_ephemeral_port(connectionSocket)
+        fileSizeBuff = recFile(dataConnection, 10)
+        print(fileSizeBuff)
+        fileSize = int(fileSizeBuff)
+        fileData = recFile(dataConnection, fileSize)
+        mkFile(cmd[1], fileData)
+        break
 
 connectionSocket.close()
+
+# class Datagram:
+#     def __init__(self,message):
+#         self.message = message
+## process message to send back to client
+# data = data.replace("client","server")
+# obj = Datagram(data.encode())
+# data = ""
